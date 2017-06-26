09c808a032c58c65eb4dcfe7d3b1d462fecea687
arthurzapata <arthuro_2004@hotmail.com>
2017-06-26 08:39:15 -0500

    acceso vendedores

--- empresa.php
+++ empresa.php
@@ -31,7 +31,7 @@ if ((isset($_GET['doLogout'])) &&($_GET['doLogout']=="true")){
 if (!isset($_SESSION)) {
   session_start();
 }
-$MM_authorizedUsers = "1,3,4";
+$MM_authorizedUsers = "1,2,3,4";
 $MM_donotCheckaccess = "false";
 
 // *** Restrict Access To Page: Grant or deny access to this page
